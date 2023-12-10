2e71cb26e0904a3e4d04d1fe8d3c0c6df282c4dc
Olonov Aleksandr <adrolv08111985@gmail.com>
2023-12-10 12:31:20 +0300

    Убрал лишнее в OOP_Sheep

--- OOP_Sheep_8kyu.py
+++ OOP_Sheep_8kyu.py
@@ -11,18 +11,3 @@ class Ship:
 
 
 Titanic = Ship(100, 20)
-=======
-class Ship:
-    """Класс корабль: наполнение и экипаж"""
-    def __init__(self, draft, crew):
-        self.draft = draft
-        self.crew = crew
-
-    def is_worth_it(self):
-        return self.draft > self.crew * 1.5 + 20
-
-
-
-Titanic = Ship(100, 20)
->>>>>>> Tasks_CodeWars/master
-print(Titanic.is_worth_it())
