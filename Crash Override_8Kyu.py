6ac3e276d810a73817e283cbf8361d39f0835d29
Olonov Aleksandr <adrolv08111985@gmail.com>
2023-12-24 13:36:24 +0300

    Псевдоним хакера 8 кю

---
+++ Crash Override_8Kyu.py
@@ -0,0 +1,7 @@
+def alias_gen(f_name, l_name):
+    try:
+    	return FIRST_NAME[f_name.upper()[0]]+' '+SURNAME[l_name.upper()[0]]
+    except:
+    	return 'Your name must start with a letter from A - Z.'
+
+
