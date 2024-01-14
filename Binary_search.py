37bc2b24688b0cce4de3da1e098221a0a1fbfe93
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-01-14 21:17:52 +0300

    Реализация бинарного поиска

---
+++ Binary_search.py
@@ -0,0 +1,27 @@
+# Бинарный поиск. [O(log n)]
+
+def binary_search(search_list, number):
+
+    low = 0
+    high = len(search_list) - 1
+
+
+    while low <= high:
+
+        mid = (low + high) // 2
+        guess = search_list[mid]
+
+        if guess == number:
+            return mid
+
+        if guess > number:
+            high = mid - 1
+
+        else:
+            low = mid + 1
+
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print(f"число стоит на позиции с индексом: {binary_search(my_list, 7)}")
